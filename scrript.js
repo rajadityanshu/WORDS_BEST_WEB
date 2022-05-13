04a33ebbd70e0772cb635898443a68e17ea0447f
rajadityanshu <104768664+rajadityanshu@users.noreply.github.com>
2022-05-13 18:15:04 +0530

    Add files via upload

---
+++ scrript.js
@@ -0,0 +1,10 @@
+var vid = document.getElementById("video");
+video=document.querySelector(".video");
+// var sound = new Audio("img/1.mp3");
+img.classList.add("imgani");
+function btn() {
+    
+    video.style.visibility="visible";
+    vid.play();
+  
+}
